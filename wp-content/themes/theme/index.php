3dbad99c07eb82ab05529ec126576dbdf6e57fbe
Clara <claradatso@gmail.com>
2019-04-26 12:29:25 +0200

    ajout fichier template partials footer.php qu'on appelle depuis index.php avec la fonction get_footer();

--- wp-content/themes/theme/index.php
+++ wp-content/themes/theme/index.php
@@ -419,22 +419,8 @@ get_header();
 	</div>
 	<!-- Contact section end-->
 
+<?php
 
-	<!-- Footer section -->
-	<footer class="footer-section">
-		<h2>2017 All rights reserved. Designed by <a href="https://colorlib.com" target="_blank">Colorlib</a></h2>
-	</footer>
-	<!-- Footer section end -->
+get_footer();
 
-
-
-
-	<!--====== Javascripts & Jquery ======-->
-	<script src="http://localhost:8888/wp-labs/wp-content/themes/theme/js/jquery-2.1.4.min.js"></script>
-	<script src="http://localhost:8888/wp-labs/wp-content/themes/theme/js/bootstrap.min.js"></script>
-	<script src="http://localhost:8888/wp-labs/wp-content/themes/theme/js/magnific-popup.min.js"></script>
-	<script src="http://localhost:8888/wp-labs/wp-content/themes/theme/js/owl.carousel.min.js"></script>
-	<script src="http://localhost:8888/wp-labs/wp-content/themes/theme/js/circle-progress.min.js"></script>
-	<script src="http://localhost:8888/wp-labs/wp-content/themes/theme/js/main.js"></script>
-</body>
-</html>
+?>
