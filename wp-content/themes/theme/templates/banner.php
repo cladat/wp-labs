b08fcb52a636973198dc9f7a1f2b7e0a36b2c0a8
Clara <claradatso@gmail.com>
2019-04-26 12:34:41 +0200

    ajout d'un fichier banner.php dans un dossier templates qui va contenir le header. on l'appelle dans l'index grâce à la fonction wordpress get_template_part('templates/banner');

---
+++ wp-content/themes/theme/templates/banner.php
@@ -0,0 +1,15 @@
+<!-- Intro Section -->
+<div class="hero-section">
+		<div class="hero-content">
+			<div class="hero-center">
+				<img src="http://localhost:8888/wp-labs/wp-content/themes/theme/img/big-logo.png" alt="">
+				<p>Get your freebie template now!</p>
+			</div>
+		</div>
+		<!-- slider -->
+		<div id="hero-slider" class="owl-carousel">
+			<div class="item  hero-item" data-bg="http://localhost:8888/wp-labs/wp-content/themes/theme/img/01.jpg"></div>
+			<div class="item  hero-item" data-bg="http://localhost:8888/wp-labs/wp-content/themes/theme/img/02.jpg"></div>
+		</div>
+	</div>
+<!-- Intro Section -->
