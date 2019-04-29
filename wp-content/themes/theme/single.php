a1e62c34d4484548f7a03c1f338bffc324f83da0
Clara <claradatso@gmail.com>
2019-04-29 12:00:41 +0200

    pour afficher le contenu on fait comme pour le titre, la fonction the_content(); sauf que cela n'affichera rien tant qu'on a pas rajouté la boucle while(have_posts());

--- wp-content/themes/theme/single.php
+++ wp-content/themes/theme/single.php
@@ -15,18 +15,18 @@ get_header();
 								<h2>03</h2>
 								<h3>Nov 2017</h3>
 							</div>
-						</div>
-						<div class="post-content">
-							<h2 class="post-title"> <?php the_title(); ?> </h2>
-							<div class="post-meta">
-								<a href="">Loredana Papp</a>
-								<a href="">Design, Inspiration</a>
-								<a href="">2 Comments</a>
-							</div>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur leo est, feugiat nec elementum id, suscipit id nulla. Phasellus vestibulum, quam tincidunt venenatis ultrices, est libero mattis ante, ac consectetur diam neque eget quam. Etiam feugiat augue et varius blandit. Praesent mattis, eros a sodales commodo.</p>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum, quam tincidunt venenatis ultrices, est libero mattis ante, ac consectetur diam neque eget quam. Etiam feugiat augue et varius blandit. Praesent mattis, eros a sodales commodo, justo ipsum rutrum mauris, sit amet egestas metus quam sed dolor. Sed consectetur, dui sed sollicitudin eleifend, arcu neque egestas lectus, sagittis viverra justo massa ut sapien. Aenean viverra ornare mauris eget lobortis. Cras vulputate elementum magna, tincidunt pharetra erat condimentum sit amet. Maecenas vitae ligula pretium, convallis magna eu, ultricies quam. In hac habitasse platea dictumst. </p>
-							<p>Fusce vel tempus nunc. Phasellus et risus eget sapien suscipit efficitur. Suspendisse iaculis purus ornare urna egestas imperdiet. Nulla congue consectetur placerat. Integer sit amet auctor justo. Pellentesque vel congue velit. Sed ullamcorper lacus scelerisque condimentum convallis. Sed ac mollis sem. </p>
-						</div>
+            </div>
+            <?php while (have_posts()) : the_post(); ?>
+              <div class="post-content">
+                <h2 class="post-title"> <?php the_title(); ?> </h2>
+                <div class="post-meta">
+                  <a href="">Loredana Papp</a>
+                  <a href="">Design, Inspiration</a>
+                  <a href="">2 Comments</a>
+                </div>
+                <?php the_content(); ?>
+              </div>
+            <?php endwhile; ?>
 						<!-- Post Author -->
 						<div class="author">
 							<div class="avatar">
