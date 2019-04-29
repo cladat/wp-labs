1ef61e30c33c5415b7f0b76b7c8490dd9af6dc7d
Clara <claradatso@gmail.com>
2019-04-29 10:08:50 +0200

    contenu dans templates + création fonctions. d'abord on sépare toutes les sections dans des fichiers templates qu'on lie dans l'index grâce à get_template_part(); ensuite on ajoute un fichier functions.php qui va avoir un add_action(); pour ajouter les css et scripts que l'on avait dans le header et footer. on les lie grâce à wp_head(); et wp_footer(); il faut bien faire attention à l'ordre des scripts pour que tout fonctionne. et renommer les noms des scripts pour pas créer de conflits avec ceux déjà existants dans wordpress

--- wp-content/themes/theme/footer.php
+++ wp-content/themes/theme/footer.php
@@ -4,13 +4,11 @@
 	</footer>
 	<!-- Footer section end -->
 
+<?php
 
-	<!--====== Javascripts & Jquery ======-->
-	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-2.1.4.min.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/magnific-popup.min.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.min.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/circle-progress.min.js"></script>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+wp_footer();
+
+?>
+
 </body>
 </html>
--- wp-content/themes/theme/functions.php
+++ wp-content/themes/theme/functions.php
@@ -14,8 +14,15 @@ function ajout_css_js()
   wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css');
   wp_enqueue_style('flaticon', get_template_directory_uri() . '/css/flaticon.css');
   wp_enqueue_style('magnific-popup', get_template_directory_uri() . '/css/magnific-popup.css');
-  wp_enqueue_style('carousel', get_template_directory_uri() . '/css/owl.carousel.css');
+  wp_enqueue_style('owl.carousel', get_template_directory_uri() . '/css/owl.carousel.css');
   wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
+
+  wp_enqueue_script('jquery-perso', get_template_directory_uri() . '/js/jquery-2.1.4.min.js', null, true);
+  wp_enqueue_script('bootstrap-perso', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('magnific-popup', get_template_directory_uri() . '/js/magnific-popup.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('circle-progress', get_template_directory_uri() . '/js/circle-progress.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('owl-carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', ['jquery-perso'], null, true);
+  wp_enqueue_script('main-js', get_template_directory_uri() . '/js/main.js', ['jquery-perso'], null, true);
 }
 // Nous ajoutons un écouteur d'événements pour nous prévenir lorsque l'on peut ajouter des css et scripts.
 // Cette écouteur va déclancher la fonction ajout_css_js()
