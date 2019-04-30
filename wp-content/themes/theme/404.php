4d55f3372d29c1298ab6aed6b3ff08d630ab98fa
Clara <claradatso@gmail.com>
2019-04-30 16:43:53 +0200

    ajout d'un fichier template 404 pour les requêtes non valides

---
+++ wp-content/themes/theme/404.php
@@ -0,0 +1,23 @@
+<?php
+
+get_header();
+?>
+
+	<!-- Page header -->
+	<div class="page-top-section">
+		<div class="overlay"></div>
+		<div class="container text-right">
+			<div class="page-info">
+				<h2>404</h2>
+				<div class="page-links">
+					<span>OOOOOOOPS! Page introuvable.</span>
+				</div>
+			</div>
+		</div>
+	</div>
+  <!-- Page header end-->
+  
+<?php
+
+get_footer();
+?>
