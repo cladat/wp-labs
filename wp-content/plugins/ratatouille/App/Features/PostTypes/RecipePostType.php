16d3985349b9e4322dba4af4232b83c860caa221
Clara <claradatso@gmail.com>
2019-05-06 10:34:20 +0200

    //PLUGIN//10. Création d'une variable slug dans la class RecipePostType

--- wp-content/plugins/ratatouille/App/Features/PostTypes/RecipePostType.php
+++ wp-content/plugins/ratatouille/App/Features/PostTypes/RecipePostType.php
@@ -11,11 +11,16 @@ namespace App\Features\PostTypes;
 
 // On à Couper Coller notre function du fichier Ratatouille.php pour l'entourer d'une class
 class RecipePostType
+
 {
+  // On créer une variable qu'on appel 'slug' on la rend public et static pour pouvoir s'en servir dans les functions de la class RecipePostType et en dehors.
+  public static $slug = 'recipe';
   public static function register()
   {
     register_post_type(
       'recipe', 
+      // on remplace le slug qui était écrit en dur par la variable, on fait cela car on va fair appel à notre function register_post_type par son indentifiant à plusieurs endroits. Si on change la valeur de la variable à tous les endroits ou son identifiant est utilisé sera mis à jours.
+      self::$slug, 
       [
            // labels contient un tableau avec pleins de paramètres par defaut en anglais comme : Add Post, Edit Post etc, on fait référence aux clefs et on y introduit une nouvelle valeur en français pour avoir les messages en français dans le backoffice.
         'labels' => [
