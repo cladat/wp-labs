a0555944c711d4f1ba32267fa9e04416cdefcf46
Clara <claradatso@gmail.com>
2019-05-06 09:54:06 +0200

    //PLUGIN//7. Création d'un alias dans ratatouille.php

--- wp-content/plugins/ratatouille/ratatouille.php
+++ wp-content/plugins/ratatouille/ratatouille.php
@@ -11,6 +11,8 @@
  *
  * @package         Ratatouille
  */
+// On use le namespace et créer un 'ALIAS' avec la commande 'as'. Ici précision que l'alias est RecipePostType mais nécéssaire que si on modifie le nom de la class. si on veut garder un Alias qui porte le meme nom que la class il ne faut pas le préciser
+use App\Features\PostTypes\RecipePostType as RecipePostType;
 
 // Your code starts here.
 
@@ -19,4 +21,4 @@
 require_once('App/Features/PostTypes/RecipePostType.php');
 
 // Appel de la class venant du namespace qu'on a définit dans le fichier RecipePostType.php ,l'avantage c'est qu'on a redéfinit le nom de notre class qui était RecipePostType par App\Features\PostTypes\RecipePostType il y a maintenant presque aucune chance qu'un autre plugin utilise le meme nom de class et donc il n'y aura pas de conflit.
-add_action('init',[App\Features\PostTypes\RecipePostType::class, 'register']);
+add_action('init',[RecipePostType::class, 'register']);
