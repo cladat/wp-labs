0cf88b688ae9ffaa9e9bd5222c5a6288120a0e08
Clara <claradatso@gmail.com>
2019-05-07 11:57:46 +0200

    //PLUGIN//43. Dans Features création sous-dossier Sections dans lequel on ajoute un nouveau fichier RecetteHomeNumber.php pour initialiser une section dans Settings>Reading (backoffice)

---
+++ wp-content/plugins/ratatouille/App/Features/Sections/RecetteHomeNumber.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Features\Sections;
+
+class RecetteHomeNumber
+{
+  /**
+   * Enregistrement de la section
+   *
+   * @return void
+   */
+  public static function init()
+  {
+    // https://developer.wordpress.org/reference/functions/add_settings_section/
+    add_settings_section(
+      'recette-home-number', // l'id (slug) de la section
+      __('Nombre de recettes sur home'), // le titre de la section qui apparaîtra
+      [self::class, 'render'], // la méthode qui affichera le contenu de la secttion
+      'reading' // l'id de la page à la quelle on ajoute la section
+    );
+  }
+  /**
+   * fonction pour render le contenu de la section
+   */
+  public static function render()
+  {
+    echo 'Ceci est ma première section';
+  }
+} 
