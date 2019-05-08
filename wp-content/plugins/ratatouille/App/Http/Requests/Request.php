c987b0b508365ccfa7a878440aaf3f0f4487e77a
Clara <claradatso@gmail.com>
2019-05-08 12:14:41 +0200

    //PLUGIN//62. on donne les instructions aux méthodes required() et email() qui vont remplir notre variable

--- wp-content/plugins/ratatouille/App/Http/Requests/Request.php
+++ wp-content/plugins/ratatouille/App/Http/Requests/Request.php
@@ -5,7 +5,7 @@ class Request{
   // On créé une variable de type array.Pour le moment on n'en fait rien.
   private static $error = array();
   // On créé une function du nom de validation qui attend un paramètre de type array.Ce paramètre va être rempli via le fichier SendMail.php ligne 44.  
-  
+
   public static function validation(array $data){
   // Pour chaque entrée on lance une des méthode ci dessous selon la valeur du tableau $data
   // Data est un tableau de clé => valeur dont la clé est le "name" de l'input et la valeur est la méthode de vérification que l'on veut appliquer sur le contenu de l'input. C'est nous qui avons fait en sorte que les valeurs du tableau $data correspondent aux names de nos inputs dans notre formulaire.
@@ -16,8 +16,17 @@ class Request{
     }
   }
 
-  public static function required(string $input){
+  public static function required(string $input_name){
+    // La methode 'validation' à redistribuer via son foreach dans l'une ou l'autre methode (required,email) selon ce que vaut '$verification' tout en remplissant notre paramètre $input_name ci dessous par la valeur $input_name rempli ligne 15 ci dessus.
+    if ($_POST[$input_name] == "") {
+      // on rempli notre tableau $error avec nos erreurs qu'on écrit ci dessous
+      self::$errors[$input_name] = sprintf(__('Le champ %s est obligatoire'), $input_name);
+    }
   }
-  public static function email(string $input){
+
+  public static function email(string $input_name){
+    if (!is_email($_POST[$input_name])) {
+      self::$errors[$input_name] = sprintf(__('Le champ %s doit être un format email'), $input_name);
+    }
   }
 } 
