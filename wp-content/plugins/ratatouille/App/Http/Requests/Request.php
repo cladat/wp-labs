01211b59ea6247a495adc813b96d690802e24a61
Clara <claradatso@gmail.com>
2019-05-08 14:42:24 +0200

    //PLUGIN//66. affichage des données de la variable 'old'. Dans Request.phpcréation de 'old' dans un foreach donc que s'il y a des erreurs. Dans send-mail.html.php déplacement de la notif en-dessous du h1 sinon bug d'affichage js

--- wp-content/plugins/ratatouille/App/Http/Requests/Request.php
+++ wp-content/plugins/ratatouille/App/Http/Requests/Request.php
@@ -14,9 +14,6 @@ class Request{
       // on lance la function de la class, 'email' ou 'required' selon ce que vaut $verification et on rempli le paramètre de la function avec $input_name
       call_user_func([self::class, $verification], $input_name);
       // En même temps qu'on fait les vérifications pour savoir si les champs sont bien remplis on en profite pour stocker ce qu'a écrit le client dans notre super global $_SESSION sous la clef 'old'.
-      // On prépare un tableau pour pouvoir renvoyer les valeurs précédentes afin de ne pas devoir les réécrire.
-      $_SESSION['old'][$input_name] = $_POST[$input_name];
-      $_SESSION;
     }
 
     // On vérifie que $errors contient quelque chose, si c'est le cas alors on récupère tous les messages d'erreurs qu'on y a stocké, on fait un foreach dessus pour réecrire chaque ligne qu'on stock dans une variable $message 
@@ -30,6 +27,11 @@ class Request{
         'status' => 'error',
         'message' => $message
       ];
+
+      // On créé $_SESSION['old'] que si il y a des erreurs
+      foreach ($data as $input_name => $validation) {
+        $_SESSION['old'][$input_name] = $_POST[$input_name];
+      }
       // on retourne sur notre page
       wp_safe_redirect(wp_get_referer());
       // Permet d'arreter le script tant qu'il y a des erreurs à partir de la ligne 44 de notre fichier SendMail.php
